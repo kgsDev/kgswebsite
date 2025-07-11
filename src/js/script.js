fe7d96fab3479244117434d894b060e61a334518
miramontron <2229622+miramontron@users.noreply.github.com>
2025-07-11 10:54:15 -0400

    adds rollover functionality for homepage

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,22 @@
 const body = document.querySelector('body');
 const menuTrigger = body.querySelector('.nav-menu-trigger');
-const subMenuTrigger = body.querySelector('.nav-sub-menu-trigger');
+const heroLinks = body.querySelectorAll('.hero-link');
 
 menuTrigger.addEventListener('click', e => {
     const navMenu = body.querySelector('.nav-menu');
     navMenu.classList.toggle('is-open')
 });
 
-subMenuTrigger.addEventListener('click', e => {
-    const navSubMenu = body.querySelector('.nav-sub-menu');
-    navSubMenu.classList.toggle('is-open');
-})
+heroLinks.forEach(link => {
+   const rollover = link.querySelector('.rollover-active');
+   link.addEventListener('mouseenter', e => {
+        rollover.style.opacity = '1';
+        rollover.style.transition = 'opacity 0.25s ease-in allow-discrete';
+
+   });
+   
+   link.addEventListener('mouseleave', e => {
+    rollover.style.opacity = '0';
+    rollover.style.transition = 'opacity 0.25s ease-out allow-discrete';
+   });
+});
